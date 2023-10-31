2ce7a77f332dbfeefc53b978d8f77087a4e01ce2
bechirMakhlouf <bechirM@proton.me>
2023-10-31 12:54:43 +0100

    fix single quote to &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <main className="border border-red-900 w-full flex justify-around">
         <div id="cta" className="flex flex-col justify-center sm:min-h-[800px] w-full">
           <h1 className="border border-white text-6xl text-center">
-            It's Time To Level Up <br /> Your Digital Game.
+            It&apos;s Time To Level Up <br /> Your Digital Game.
           </h1>
-          <button className="border border-white">Let's Chat</button>
+          <button className="border border-white">Let&apos;s Chat</button>
         </div>
 
         <div className="flex justify-center items-center w-full border border-white">
